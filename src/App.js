8369b07aee2be4ee8c45d92925479b4f98f50220
Trey Coggins <trey@surfturkey.net>
2023-10-07 11:20:41 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component.jsx";
+import SearchBox from "./components/search-box/search-box.component.jsx";
+class App extends Component {
+	constructor() {
+		super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		this.state = {
+			monsters: [],
+			searchField: "",
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(() => {
+					return { monsters: users };
+				})
+			);
+	}
+
+	onSearchChange = (event) => {
+		console.log(event.target.value);
+		const searchField = event.target.value.toLocaleLowerCase();
+
+		this.setState(() => {
+			return { searchField };
+		});
+	};
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
+		return (
+			<div className="App">
+			<h1 className="app-title">Monsters Rolodex</h1>
+				<SearchBox
+					onChangeHandler={onSearchChange}
+					placeholder="search monsters"
+					className="monsters-search-box"
+				/>
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
